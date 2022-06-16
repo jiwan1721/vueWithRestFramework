e9ef66161a3df8af06d5add9bfd6349f022d0369
jiwan chanda <chandjiwn9@gmail.com>
2022-06-16 18:07:23 +0545

    basics

---
+++ src/components/PrintValues.vue
@@ -0,0 +1,40 @@
+<template>
+    <div v-for="task in tasks"> 
+
+          <div class="content">
+            {{ task.id }} 
+
+        </div>
+    </div>
+</template>
+<script>
+import axios from 'axios'
+import { response } from 'express';
+    export default {
+        name:'PrintValues',
+        data(){
+            return {
+                tasks: []
+            }
+        },
+        mounted(){
+            this.get_Tasks();
+        },
+        methods: {
+            get_Tasks(){
+                axios({
+                    method:'get',
+                    url:'http://127.0.0.1:8000/user/',
+                    auth: {
+                        username:'your name',
+                        password:'your Password'
+                    }
+                }).then(response=>this.tasks =response.data);
+            }
+        }
+    }
+
+</script>
+<style scoped>
+
+</style>
